912ec3847f8e2a47df1eeafa5506427cfe05a878
Sem Star <144341688+Sem1150030@users.noreply.github.com>
2025-11-01 13:17:16 +0100

    Created the dashboard page

--- starfitness/routes/web.php
+++ starfitness/routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-    Route::get('/', function () {
-        return view('index.index');
-    });
+Route::get('/', function () {
+    return view('index.index');
+});
 
-    Route::get('/auth/register', [AuthController::class, 'register'])->name('auth.register');
-    Route::get('/auth/login', [AuthController::class, 'login'])->name('auth.login');
+Route::get('/auth/register', [AuthController::class, 'register'])->name('auth.register');
+Route::get('/auth/login', [AuthController::class, 'login'])->name('auth.login');
+
+Route::middleware(['ensure.auth'])->group(function () {
+    Route::get('home/dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
+});
